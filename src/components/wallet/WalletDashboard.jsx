e8a4bad80d23488224ebf798bcf84236d7017cf2
Anthony Fennell <paintingshitwithtony@gmail.com>
2026-01-08 00:13:01 +0000

    File changes

--- src/components/wallet/WalletDashboard.jsx
+++ src/components/wallet/WalletDashboard.jsx
@@ -508,7 +508,7 @@ export default function WalletDashboard({ account, onLogout }) {
             </div>
 
             {/* Spacer for fixed header */}
-            <div className="h-20 md:h-24"></div>
+            <div className="h-28 md:h-32"></div>
 
             {/* Balance Card */}
             <motion.div
